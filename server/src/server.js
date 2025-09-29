b5a5a033b3ee9aec9bcdb5aac90dcb6dbcab9ebb
Phạm Diệu <phamdieu1503@gmail.com>
2025-09-29 23:38:34 +0700

    update origin

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: process.env.DOMAIN_URL,
         credentials: true,
     }),
 );
